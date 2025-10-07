8dcf11182932cacbc965a4e139b12da7db3736cd
liuz-blip <liuz@stpaulsschool.org.uk>
2025-10-07 23:17:35 +0100

    C# String Homework

---
+++ Code/C# Strings Real/anagramCheck/anagramCheck/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int anagramCheck(string checkStr, List<string> toCheckStr)
+        {
+            int anagrams = 0;
+            char[] mainWord = checkStr.ToCharArray();
+            Array.Sort(mainWord);
+            string mainWordStr = new string(mainWord);
+
+
+            foreach (string word in toCheckStr)
+            {
+                char[] letters = word.ToCharArray();
+                Array.Sort(letters);
+                string sortedWordStr = new string(letters);
+                
+                if (mainWordStr == sortedWordStr)
+                {
+                    anagrams++;
+                }
+            }
+
+            return anagrams;
+        }
+
+        Console.WriteLine(anagramCheck("star", new List<string> { "rats", "arts", "arc" }));
+    }
+}
---
+++ Code/C# Strings Real/varaibleNameHelper/variableNameHelper/Program.cs
@@ -0,0 +1,89 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+
+        string camelCase(List<string> toConvStr)
+        {
+            string convedStr = "";
+            convedStr += toConvStr[0].ToLower();
+
+            for (int i = 1; i < toConvStr.Count(); i++)
+            {
+                string word = toConvStr[i].ToLower();
+                string newWord = "";
+                newWord += char.ToUpper(word[0]);
+                newWord += word.Substring(1, word.Length - 1);
+                convedStr += newWord;
+            }
+            return convedStr;
+        }
+
+
+        string PascalCase(List<string> toConvStr)
+        {
+            string convedStr = "";
+
+            foreach (string unprocessedWord in toConvStr)
+            {
+                string newWord = "";
+                string word = unprocessedWord.ToLower();
+                newWord += char.ToUpper(word[0]);
+                newWord += word.Substring(1, word.Length - 1);
+                convedStr += newWord;
+            }
+            return convedStr;
+        }
+
+
+        string snake_case(List<string> toConvStr)
+        {
+            string convedStr = "";
+
+            for (int i = 0; i < toConvStr.Count - 1; i++)
+            {
+                string word = toConvStr[i];
+                convedStr += word.ToLower() + "_";
+            }
+            convedStr += toConvStr[toConvStr.Count() - 1].ToLower();
+
+            return convedStr;
+        }
+
+
+        string VariableNameHelper(string toConvertStr, int choice)
+        {
+            // Choices are:
+            //     1. camelCase
+            //     2. PascalCase
+            //     3. snake_case
+
+            string convertedStr = "";
+            List<string> toConvert = new List<string>(toConvertStr.Split(" "));
+
+            switch (choice)
+            {
+                case 1:
+                    convertedStr = camelCase(toConvert);
+                    break;
+                case 2:
+                    convertedStr = PascalCase(toConvert);
+                    break;
+                case 3:
+                    convertedStr = snake_case(toConvert);
+                    break;
+
+            }
+
+            return convertedStr;
+        }
+
+        Console.WriteLine(VariableNameHelper("nEw fuNcTiOn", 2));
+
+
+    }
+}
