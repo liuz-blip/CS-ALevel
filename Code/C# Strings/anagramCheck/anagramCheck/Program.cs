d7e765a224e1c84aec5259648210406b9e460444
liuz-blip <liuz@stpaulsschool.org.uk>
2025-10-07 22:57:41 +0100

    C# Strings Homework

--- Code/C# Strings/anagramCheck/anagramCheck/Program.cs
+++ Code/C# Strings/anagramCheck/anagramCheck/Program.cs
@@ -1,29 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        
         int anagramCheck(string checkStr, List<string> toCheckStr)
         {
             int anagrams = 0;
+            char[] mainWord = checkStr.ToCharArray();
+            Array.Sort(mainWord);
+            string mainWordStr = new string(mainWord);
+
 
             foreach (string word in toCheckStr)
             {
-                List<char> letters = new List<char>();
-                foreach (char c in word)
+                char[] letters = word.ToCharArray();
+                Array.Sort(letters);
+                string sortedWordStr = new string(letters);
+                
+                if (mainWordStr == sortedWordStr)
                 {
-                    letters.Add(c);
+                    anagrams++;
                 }
             }
 
             return anagrams;
         }
 
-
+        Console.WriteLine(anagramCheck("star", new List<string> { "rats", "arts", "arc" }));
     }
 }
-
-
